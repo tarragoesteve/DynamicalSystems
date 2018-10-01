14b7050e8fa879144629c634dd7b57c8dc3bf9af
tarragoesteve <tarragoesteve@gmail.com>
2018-10-02 00:22:54 +0200

    modified mod function

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import * as System from "./system";
 let mySystem = new System.System(`
+  function mod(n, m) {
+    return ((n % m) + m) % m;
+  }
   var x = input[0] + parameters.a * Math.sin(input[0] + input[1]);
   var y = input[0] + input[1];
-  x = ((x + Math.PI) % (2 * Math.PI)) - Math.PI;
-  y = ((y + Math.PI) % (2 * Math.PI)) - Math.PI;
+  x = mod(x + Math.PI,2 * Math.PI) - Math.PI;
+  y = mod(y + Math.PI,2 * Math.PI) - Math.PI;
   return [x,y];
 `,[[-Math.PI,Math.PI],[-Math.PI,Math.PI]],
 '{"a":-0.7}');
-//mySystem.voidOrbitDraw()
-mySystem.voidKPeriodDraw(4)
+mySystem.voidOrbitDraw()
+//mySystem.voidKPeriodDraw(4)
